3b35deaa30a56b7f36ba40ee7eccbfcaece7455d
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-30 12:09:52 +0530

    df

--- src/main/java/com/SprintBootLearning/LearningSB/HelloController.java
+++ src/main/java/com/SprintBootLearning/LearningSB/HelloController.java
@@ -1,4 +1,22 @@
 package com.SprintBootLearning.LearningSB;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class HelloController {
+    @GetMapping("/rohit")
+    public String hello()
+        {
+            return "Hello World! ramiya";
+        }
+
+        @PostMapping("/rohit")
+    public String name(@RequestBody String name)
+        {
+            return "Hello "+name+"!";
+        }
 }
